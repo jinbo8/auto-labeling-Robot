2b23f0ac176009f6e80b0f7886c2778ed7dfb27c
jinbowang <2658773364@qq.com>
2026-08-29 18:08:12 +0800

    update: 2026-08-29 18:08:12

---
+++ README/4.有关开源项目.py
@@ -0,0 +1,9 @@
+# 1.github 项目地址
+
+https://github.com/facebookresearch/sam2
+
+https://github.com/cvat-ai/cvat
+https://github.com/CVHub520/X-AnyLabeling
+https://github.com/HumanSignal/label-studio
+
+
